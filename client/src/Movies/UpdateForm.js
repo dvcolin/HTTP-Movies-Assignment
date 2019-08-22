5a5391577e8472ac621b9d40fc0d07024737f4a4
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-08-22 12:15:36 -0700

    added formik form in UpdateForm component

--- client/src/Movies/UpdateForm.js
+++ client/src/Movies/UpdateForm.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import { Form, Field, withFormik } from 'formik'
 
 const UpdateForm = () => {
     return (
         <div>
-            <h1>Hello</h1>
+            <h1>Update Movie</h1>
+            <Form>
+                <Field type='text' name='title'></Field>
+                <Field type='text' name='director'></Field>
+                <Field type='text' name='metascore'></Field>
+                <Field type='text' name='title'></Field>
+            </Form>
         </div>
     )
 }
 
-export default UpdateForm
+const FormikForm = withFormik({
+    mapPropsToValues({ title, director, metascore, stars }) {
+        return {
+            title: title || '',
+            director: director || '',
+            metascore: metascore || '',
+            stars: stars || [],
+        }
+    }
+})(UpdateForm)
+
+
+export default FormikForm
