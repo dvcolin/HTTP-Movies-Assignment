5804e7e80cd53cc38d13c321659927b169785357
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-08-23 03:51:43 -0700

    added functionality to edit movies from form

--- client/src/Movies/UpdateForm.js
+++ client/src/Movies/UpdateForm.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios';
 
-const UpdateForm = () => {
+const UpdateForm = props => {
+
+    const [movie, setMovie] = useState({})
+    const [stars, setStars] = useState([]);
+
+useEffect(() => {
+    const id = props.match.params.id;
+    const movieInArr = props.movies.find(movie => movie.id === parseInt(id));
+    if(movieInArr) {
+        setMovie(movieInArr);
+        setStars(movieInArr.stars);
+    }
+}, [])
+
+const handleChanges = e => {
+    setMovie({...movie, [e.target.name]: e.target.value})
+}
+
+const updateMovie = e => {
+    e.preventDefault();
+    Axios
+    .put(`http://localhost:5000/api/movies/${movie.id}`, movie)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+    
+}
+
     return (
         <div>
-            <h1>Hello</h1>
+            <h1>Update Movie</h1>
+            <form>
+                <input onChange={handleChanges} type='text' name='title' value={movie.title}></input>
+                <input onChange={handleChanges} type='text' name='director' value={movie.director}></input>
+                <input onChange={handleChanges} type='text' name='metascore' value={movie.metascore}></input>
+                <button onClick={updateMovie} type='submit'>Update</button>
+            </form>
         </div>
     )
 }
